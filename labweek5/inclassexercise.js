8ee9f723bac377af9fc727994ddbd0a412d2d726
Erin Rusek <erinrusek@Erins-MacBook-Pro.local>
2020-02-24 17:42:12 -0500

    studio week 6

---
+++ labweek5/inclassexercise.js
@@ -0,0 +1,12 @@
+function changeHeading (color) {
+	document.getElementById("2").style.backgroundColor='red';
+}
+
+// 1st part is getting ID
+// 2nd is calling the heading
+// 3rd part is the DOM
+
+
+function changeHeading (color) {
+	document.getElementById("heading").innerHTML='A JavaScript Compendium';
+}
---
+++ labweek5/main.js
@@ -0,0 +1,17 @@
+console.log('ay carumba');
+
+
+function printWord () {
+	console.log('Radiohead is like the best band ever');
+	console.log('but honestly Coldplays older work is pretty good too');
+	console.log('This is the no judgement console');
+}
+
+function printWord2 () {
+	console.log('BRRRRRRRAAAP');
+	
+}
+
+function changeHeading () {
+	document.getElementById('heading').innerHTML="i clicked the button"
+}
