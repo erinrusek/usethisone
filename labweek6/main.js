3923c246fb75e8b4d4013b09f2038d0d819feb36
Erin Rusek <erinrusek@Erins-MacBook-Pro.local>
2020-02-28 14:51:26 -0500

    JS 1 AHHHHH

---
+++ labweek6/main.js
@@ -0,0 +1,31 @@
+// function myFunction(innerHTML) {
+//   document.getElementById("h1").innerHTML = "01011010 01100101 01110010 01101111 01100101 01110011 00100000 01100001 01101110 01100100 00100000 01001111 01101110 01100101 01110011 00101100 00100000 01010011 01100001 01100100 01101001 01100101 00100000 01010000 01101100 01100001 01101110 01110100 00001010 ";
+// }
+
+
+// function myFunction() {
+//   document.getElementById("p1").object.onmousemove = "HEY";
+// }
+
+// function myFunction(bep) {
+//   document.body.style.backgroundColor = "#39bee3";
+// }
+
+function myFunction() {
+  document.body.style.backgroundColor = "#3dd191";
+}
+
+function bigTxt(x) {
+  x.style.height = "64%";
+  x.style.width = "64%";
+}
+
+function normalTxt(x) {
+  x.style.height = "20%";
+  x.style.width = "20%";
+}
+
+// function myFunction() {
+//   document.getElementById("thinking").style.cursor = "progress";
+// }
+
