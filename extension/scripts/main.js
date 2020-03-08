6bef5c750fbf0003f2a114ebbc0ab25506d3f59d
Erin Rusek <erinrusek@Erins-MacBook-Pro.local>
2020-03-08 16:32:30 -0400

    studio week 8

---
+++ extension/scripts/main.js
@@ -0,0 +1,3 @@
+// $('div').css({"background-color":"pink"});
+
+$('img').attr({'src': chrome.extension.getURL("img/potato1.jpg")});
---
+++ studioweek8/main.js
@@ -0,0 +1,1 @@
+document.body.style.backgroundColor = "#0ebec4";
