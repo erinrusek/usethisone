58167e59d9ea7da507c03f6bd0ce75fbf3677488
Your Name <you@example.com>
2020-04-17 14:03:38 -0500

    mousedown practice

---
+++ labweek12/main.js
@@ -0,0 +1,97 @@
+var timeline = [
+	{
+		"name": "Sahelanthropus Tchadensis",
+		"years": "7-6 MYA",
+		"location": "Africa",
+		"shape": "round",
+		"gif": "<img src='https://insider.si.edu/wp-content/uploads/2011/03/NHB2011-00358.jpg'>"
+	},
+
+	{
+		"name": "Australopithecus Afarensis",
+		"years": "3.9-2.9 MYA",
+		"location": "Africa",
+		"shape": "curved",
+		"gif": "<img src='https://i.pinimg.com/originals/eb/71/0f/eb710f16c8db02de773e7ebee740a43b.jpg'>"
+	},
+
+	{
+		"name": "Australopithecus Africanus",
+		"years": "3-2 MYA",
+		"location": "Africa",
+		"shape": "round",
+		"gif": "<img src='https://images.squarespace-cdn.com/content/v1/55ed84aee4b09f830fd03855/1517406417248-AJX1F4JHHWQU2CJZS28R/ke17ZwdGBToddI8pDm48kCU9r-vGp3mOucjGZracXy17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0jRb3i7UjwNNySrgFE_nWasCraMke_CSdy6UoEdDzAjZf_SGFDuDxk3eOQYknLEP5Q/Australopithecus+africanus+w+closed+mouth+IMG_9863a+light.jpg'>"
+	},
+
+	{
+		"name": "Homo Habilis",
+		"years": "2.1-1.5 MYA",
+		"location": "sub-Saharan Africa",
+		"shape": "round",
+		"gif": "<img src='https://images.squarespace-cdn.com/content/v1/55ed84aee4b09f830fd03855/1514923505899-UZJG5SY0TPE9BT6WMWE0/ke17ZwdGBToddI8pDm48kG_YwUfLjpS7zzgj7nELvah7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UUYvgDk6xUb5D6B8d2NscAYOhEgtp9hU0Pn_T2-zVO1MQjT9byXZM3ISxo3y1NRptg/Image+600+H+habilis+sensu+lato+based+on+KNM+ER+1470+%28rudolfensis%29+lo.jpg?format=1500w'>"
+	},
+
+	{
+		"name": "Homo Georgicus",
+		"years": "1.8 MYA",
+		"location": "southern Caucasus region of the Republic of Georgia",
+		"shape": "oval",
+		"gif": "<img src='https://lh3.googleusercontent.com/proxy/zL8EYp6TffUs8QvwnZlSU9hc4yfYZIw_Hy5odqLc0RPzPb1vF5gMJi4wXkXAYQxZ2AbfzObzUnihpM4ZMTNdXvGDsafTNcqJROnqDP8kyG5ISUD-35EW0SFaPqzKOJh-lVErRQ'>"
+	},
+
+	{
+		"name": "Homo Ergaster",
+		"years": "1.9-1.4 MYA",
+		"location": "eastern and southern Africa",
+		"shape": "round",
+		"gif": "<img src='https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/H_habilis_front_26231_afeft0.jpg'>"
+	},
+
+	{
+		"name": "Homo Erectus",
+		"years": "2 MYA-100,000 YA",
+		"location": "South Africa, Kenya, Spain, China, and Java (Indonesia)",
+		"shape": "round",
+		"gif": "<img src='https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/H_erectus_f1_29078_zn6uog.jpg'>"
+	},
+
+	{
+		"name": "Homo Antecessor",
+		"years": "1.2-0.8 MYA",
+		"location": " Western Europe (Spain, England and France)",
+		"shape": "diamond",
+		"gif": "<img src='https://upload.wikimedia.org/wikipedia/commons/2/2b/Homo-antecessor-facial-reconstruction.jpg'>"
+	},
+
+	{
+		"name": "Homo Heidelbergensis",
+		"years": "700,000 to 300,000 YA",
+		"location": "Africa, Europe, and possibly Asia",
+		"shape": "pear shape",
+		"gif": "<img src='https://upload.wikimedia.org/wikipedia/commons/9/96/Homo_heidelbergensis_adult_male_-_head_model_-_Smithsonian_Museum_of_Natural_History_-_2012-05-17.jpg'>"
+	},
+
+	{
+		"name": "Homo Floresiensis",
+		"years": "18,000 YA",
+		"location": "Flores, Indonesia",
+		"shape": "round",
+		"gif": "<img src='https://inews.co.uk/images-i.jpimedia.uk/imagefetch/https://inews.co.uk/wp-content/uploads/2016/09/ho-fo.jpg'>"
+	},
+
+	{
+		"name": "Homo Neanderthalensis",
+		"years": "130,000-40,000 YA",
+		"location": "Eurasia, as far north and west as the Britain, through part of the Middle East, to Uzbekistan",
+		"shape": "round",
+		"gif": "<img src='https://mappingignorance.org/fx/media/2014/05/The-Twilight-of-the-Neanderthals.jpg'>"
+	},
+
+	{
+		"name": "Homo Sapiens",
+		"years": "50,000 YA-PRESENT",
+		"location": "Worldwide",
+		"shape": "round",
+		"gif": "<img src='https://www.wfla.com/wp-content/uploads/sites/71/2020/03/Clearwater-beach.jpg'>"
+	}
+]
