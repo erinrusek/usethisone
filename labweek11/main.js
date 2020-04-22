6dd7b4619da0f9e2ecff4e602c95d772e60fa8f1
Your Name <you@example.com>
2020-04-22 14:41:59 -0500

    final process commit 1

---
+++ labweek11/main.js
@@ -0,0 +1,118 @@
+var photos = [
+
+		
+	{
+		"pic": "<img src='1.png'>",
+		"name": "7-6 MYA",
+		"size": '<style="width:500px;height:600px;">'
+		
+	},
+
+	{
+		"pic": "<img src='2.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='3.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='4.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='5.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='6.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='7.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='8.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='9.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='10.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='11.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='12.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='13.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='14.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='15.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='16.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='17.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='18.png'>",
+		"name": "7-6 MYA",
+		
+	},
+
+	{
+		"pic": "<img src='19.jpg'>",
+		"name": "7-6 MYA",
+		
+	}
+]
---
+++ labweek13/biomes.js
@@ -0,0 +1,26 @@
+var fruits = [
+	{
+		"name": "blueberry",
+		"color": "blue",
+		"flavor": "tart",
+		"physicalAttributes": {
+			"shape": "round",
+			"robustness": "delicate",
+			"size": "small",
+			"density": "low"
+		},
+		"recipes": ["blueberry pancakes", "oatmeal with blueberries", "blueberry jam"]
+	},
+
+	{
+		"name": "banana",
+		"color": "yellow",
+		"flavor": "sweet",
+		"physicalAttributes": {
+			"shape": "curved",
+			"robustness": "delicate",
+			"density": "high"
+		},
+		"recipes": ["banana bread", "banana ice cream", "bananas on toast", "banana pudding"]
+	}
+]
