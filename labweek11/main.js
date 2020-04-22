3efe2680fef8edf1bdc8bfd31e0cb62183b7bb76
Your Name <you@example.com>
2020-04-22 15:53:33 -0500

    second commit rerouted links + audio

--- labweek11/main.js
+++ labweek11/main.js
@@ -3,116 +3,116 @@ var photos = [
 		
 	{
 		"pic": "<img src='1.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 1",
 		"size": '<style="width:500px;height:600px;">'
 		
 	},
 
 	{
 		"pic": "<img src='2.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 2",
 		
 	},
 
 	{
 		"pic": "<img src='3.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 3",
 		
 	},
 
 	{
 		"pic": "<img src='4.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 4",
 		
 	},
 
 	{
 		"pic": "<img src='5.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 5",
 		
 	},
 
 	{
 		"pic": "<img src='6.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 6",
 		
 	},
 
 	{
 		"pic": "<img src='7.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 7",
 		
 	},
 
 	{
 		"pic": "<img src='8.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 8",
 		
 	},
 
 	{
 		"pic": "<img src='9.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 9",
 		
 	},
 
 	{
 		"pic": "<img src='10.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 10",
 		
 	},
 
 	{
 		"pic": "<img src='11.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 11",
 		
 	},
 
 	{
 		"pic": "<img src='12.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 12",
 		
 	},
 
 	{
 		"pic": "<img src='13.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 13",
 		
 	},
 
 	{
 		"pic": "<img src='14.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 14",
 		
 	},
 
 	{
 		"pic": "<img src='15.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 15",
 		
 	},
 
 	{
 		"pic": "<img src='16.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 16",
 		
 	},
 
 	{
 		"pic": "<img src='17.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 17",
 		
 	},
 
 	{
 		"pic": "<img src='18.png'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 18",
 		
 	},
 
 	{
 		"pic": "<img src='19.jpg'>",
-		"name": "7-6 MYA",
+		"name": "Quarantine Day: 19",
 		
 	}
 ]
