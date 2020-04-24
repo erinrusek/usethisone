3eb3d5ff2f05a1649ee3be9d51ab5733531cd5b2
Your Name <you@example.com>
2020-04-24 02:36:23 -0500

    lab week 13

--- labweek11/main.js
+++ labweek11/main.js
@@ -4,6 +4,7 @@ var photos = [
 	{
 		"pic": "<img src='1.png'>",
 		"name": "Quarantine Day: 1",
+		"desc": "Our first online class in Intro to Deign studies recitation using the director of the communication design department teaching his class in cyberspace as a background";
 		"size": '<style="width:500px;height:600px;">'
 		
 	},
@@ -11,96 +12,123 @@ var photos = [
 	{
 		"pic": "<img src='2.png'>",
 		"name": "Quarantine Day: 2",
+		"desc": "Teacher pops into our breakout room to make sure we're working, and finds this meta behavior instead";
+
 		
 	},
 
 	{
 		"pic": "<img src='3.png'>",
 		"name": "Quarantine Day: 3",
-		
+		"desc": "Zoom calling friends for social interaction and background image battles ensue....";
+
+
 	},
 
 	{
 		"pic": "<img src='4.png'>",
 		"name": "Quarantine Day: 4",
+		"desc": "Zoom calling friends for social interaction and background image battles ensue....";
+
 		
 	},
 
 	{
 		"pic": "<img src='5.png'>",
 		"name": "Quarantine Day: 5",
+		"desc": "Zoom calling friends for social interaction and background image battles ensue....";
 		
 	},
 
 	{
 		"pic": "<img src='6.png'>",
 		"name": "Quarantine Day: 6",
+		"desc": "Zoom calling friends for social interaction and background image battles ensue....";
 		
 	},
 
 	{
 		"pic": "<img src='7.png'>",
 		"name": "Quarantine Day: 7",
+		"desc": "Roon knows about the time you used CSS kayframes instead of linking to JavaScript to create your time-based animation";
 		
 	},
 
 	{
 		"pic": "<img src='8.png'>",
 		"name": "Quarantine Day: 8",
+		"desc": "Qu'est-ce que c'est???";
+
 		
 	},
 
 	{
 		"pic": "<img src='9.png'>",
 		"name": "Quarantine Day: 9",
+		"desc": "Cursed image radiating more meta energy (mirror images on the 2nd and last participant screens)";
+
 		
 	},
 
 	{
 		"pic": "<img src='10.png'>",
 		"name": "Quarantine Day: 10",
-		
+		"desc": "No comment";
+
 	},
 
 	{
 		"pic": "<img src='11.png'>",
 		"name": "Quarantine Day: 11",
+		"desc": "The discovery of the 'I have a green screen' effect on Zoom";
+
 		
 	},
 
 	{
 		"pic": "<img src='12.png'>",
 		"name": "Quarantine Day: 12",
+		"desc": "Which ones are from the South?";
+
 		
 	},
 
 	{
 		"pic": "<img src='13.png'>",
 		"name": "Quarantine Day: 13",
+		"desc": "No comment";
+
 		
 	},
 
 	{
 		"pic": "<img src='14.png'>",
 		"name": "Quarantine Day: 14",
-		
+		"desc": "The discovery of dad backgrounds holds infinite potential";
+
 	},
 
 	{
 		"pic": "<img src='15.png'>",
 		"name": "Quarantine Day: 15",
+		"desc": "No comment";
+
 		
 	},
 
 	{
 		"pic": "<img src='16.png'>",
 		"name": "Quarantine Day: 16",
+		"desc": "When you know no one can see your drawing but you're trying your best";
+
 		
 	},
 
 	{
 		"pic": "<img src='17.png'>",
 		"name": "Quarantine Day: 17",
+		"desc": "Ｔｈｅ ｂｅｓｔ ｂａｎｄ ｅｖｅｒ， ｅｖｅｒｙｏｎｅ ａｇｒｅｅｓ";
+
 		
 	},
 
--- labweek13/biomes.js
+++ labweek13/biomes.js
@@ -1,26 +1,38 @@
-var fruits = [
+var biomes = [
 	{
-		"name": "blueberry",
-		"color": "blue",
-		"flavor": "tart",
-		"physicalAttributes": {
-			"shape": "round",
-			"robustness": "delicate",
-			"size": "small",
-			"density": "low"
+		"name": "<br><br><h2>Tundra: </h2><br><br>",
+		"climate": "<br><br>transitional climate between the Subarctic and Ice cap climates",
+		"plants": "<br><br>Composition: short shrubs, sedges, grasses, flowers, birch trees and willow trees.",
+		"factAttributes": {
+			"one": "The word tundra comes from a Finnish word tunturi, which means treeless plain or barren land.",
+			"two": "The tundra is a very fragile biome that is shrinking as the permafrost melts.",
+			"three": "Plants that grow in tight groups to protect themselves from the cold are sometimes called cushion plants.",
+			"four": "the tundra acts as a sort of protective sink in which plants growing during the summer trap carbon dioxide when they are later frozen in permafrost during the winter months."
 		},
-		"recipes": ["blueberry pancakes", "oatmeal with blueberries", "blueberry jam"]
+		"countries": ["<br><br><br><br>Canada", "--Greenland", "--Iceland", "--Norway", "--Russia", "--Alaska"]
 	},
 
 	{
-		"name": "banana",
-		"color": "yellow",
-		"flavor": "sweet",
-		"physicalAttributes": {
-			"shape": "curved",
-			"robustness": "delicate",
-			"density": "high"
+		"name": "<br><br><h2>Desert: </h2><br><br>",
+		"climate": "<br><br>excess of evaporation over precipitation. The typically bald, rocky, or sandy surfaces in desert climates hold little moisture and evaporate the little rainfall they receive.",
+		"plants": "<br><br>Composition: Cacti and Succulents, Wildflowers, and Trees, Shrubs, and Grasses",
+		"factAttributes": {
+			"one": "A desert receives less than 10 inches of rain a year.",
+			"two": "Antarctica is the largest cold desert on Earth.",
+			"three": "20 percent of the world's land surface is desert."
 		},
-		"recipes": ["banana bread", "banana ice cream", "bananas on toast", "banana pudding"]
+		"countries": ["<br><br><br><br>Algeria", "--Chad", "--Egypt", "--Mali", "--Sudan", "--Tunisia", "--Australia", "--United States", "--Afghanistan", "--China", "--Chile", "--Antarctica"]
+	},
+
+	{
+		"name": "<br><br><h2>Grassland: </h2><br><br>",
+		"climate": "<br><br>The grassland climate is rather dry, averaging about 20 to 100 centimeters (8–40 inches) of precipitation a year. Summers are very hot and may reach 45°C (113°F). Winter temperatures often fall below freezing, which is 0°C (32°F).",
+		"plants": "<br><br>Composition: GRASS. ex: purple needlegrass, wild oats, foxtail, ryegrass, and buffalo grass",
+		"factAttributes": {
+			"one": "Grasslands go by many names. In the U.S. Midwest, they're often called prairies. In South America, they're known as pampas. Central Eurasian grasslands are referred to as steppes, while African grasslands are savannas.",
+			"two": " Grassland biomes are normally situated between a forest and a desert. In fact, grasslands surround every desert in Asia.",
+			"three": "Twenty-five percent of the Earth is covered by the grassland biome."
+		},
+		"countries": ["<br><br><br><br>United States", "--Africa", "--Eurasia", "--South America"]
 	}
 ]
