accdb1bea1c96b4eb61678756d1a7fc1915d61a8
Your Name <you@example.com>
2020-04-27 21:42:57 -0500

    process link 1

--- labweek11/main.js
+++ labweek11/main.js
@@ -4,15 +4,15 @@ var photos = [
 	{
 		"pic": "<img src='1.png'>",
 		"name": "Quarantine Day: 1",
-		"desc": "Our first online class in Intro to Deign studies recitation using the director of the communication design department teaching his class in cyberspace as a background";
-		"size": '<style="width:500px;height:600px;">'
+		"desc": "Our first online class in Intro to Deign studies recitation using the director of the communication design department teaching his class in cyberspace as a background",
+		"size": "",
 		
 	},
 
 	{
 		"pic": "<img src='2.png'>",
 		"name": "Quarantine Day: 2",
-		"desc": "Teacher pops into our breakout room to make sure we're working, and finds this meta behavior instead";
+		"desc": "Teacher pops into our breakout room to make sure we're working, and finds this meta behavior instead",
 
 		
 	},
@@ -20,7 +20,7 @@ var photos = [
 	{
 		"pic": "<img src='3.png'>",
 		"name": "Quarantine Day: 3",
-		"desc": "Zoom calling friends for social interaction and background image battles ensue....";
+		"desc": "Zoom calling friends for social interaction and background image battles ensue....",
 
 
 	},
@@ -28,7 +28,7 @@ var photos = [
 	{
 		"pic": "<img src='4.png'>",
 		"name": "Quarantine Day: 4",
-		"desc": "Zoom calling friends for social interaction and background image battles ensue....";
+		"desc": "Zoom calling friends for social interaction and background image battles ensue....",
 
 		
 	},
@@ -36,28 +36,28 @@ var photos = [
 	{
 		"pic": "<img src='5.png'>",
 		"name": "Quarantine Day: 5",
-		"desc": "Zoom calling friends for social interaction and background image battles ensue....";
+		"desc": "Zoom calling friends for social interaction and background image battles ensue....",
 		
 	},
 
 	{
 		"pic": "<img src='6.png'>",
 		"name": "Quarantine Day: 6",
-		"desc": "Zoom calling friends for social interaction and background image battles ensue....";
+		"desc": "Zoom calling friends for social interaction and background image battles ensue....",
 		
 	},
 
 	{
 		"pic": "<img src='7.png'>",
 		"name": "Quarantine Day: 7",
-		"desc": "Roon knows about the time you used CSS kayframes instead of linking to JavaScript to create your time-based animation";
+		"desc": "Roon knows about the time you used CSS kayframes instead of linking to JavaScript to create your time-based animation",
 		
 	},
 
 	{
 		"pic": "<img src='8.png'>",
 		"name": "Quarantine Day: 8",
-		"desc": "Qu'est-ce que c'est???";
+		"desc": "Qu'est-ce que c'est???",
 
 		
 	},
@@ -65,7 +65,7 @@ var photos = [
 	{
 		"pic": "<img src='9.png'>",
 		"name": "Quarantine Day: 9",
-		"desc": "Cursed image radiating more meta energy (mirror images on the 2nd and last participant screens)";
+		"desc": "Cursed image radiating more meta energy (mirror images on the 2nd and last participant screens)",
 
 		
 	},
@@ -73,14 +73,14 @@ var photos = [
 	{
 		"pic": "<img src='10.png'>",
 		"name": "Quarantine Day: 10",
-		"desc": "No comment";
+		"desc": "No comment",
 
 	},
 
 	{
 		"pic": "<img src='11.png'>",
 		"name": "Quarantine Day: 11",
-		"desc": "The discovery of the 'I have a green screen' effect on Zoom";
+		"desc": "The discovery of the 'I have a green screen' effect on Zoom",
 
 		
 	},
@@ -88,7 +88,7 @@ var photos = [
 	{
 		"pic": "<img src='12.png'>",
 		"name": "Quarantine Day: 12",
-		"desc": "Which ones are from the South?";
+		"desc": "Which ones are from the South?",
 
 		
 	},
@@ -96,7 +96,7 @@ var photos = [
 	{
 		"pic": "<img src='13.png'>",
 		"name": "Quarantine Day: 13",
-		"desc": "No comment";
+		"desc": "No comment",
 
 		
 	},
@@ -104,14 +104,14 @@ var photos = [
 	{
 		"pic": "<img src='14.png'>",
 		"name": "Quarantine Day: 14",
-		"desc": "The discovery of dad backgrounds holds infinite potential";
+		"desc": "The discovery of dad backgrounds holds infinite potential",
 
 	},
 
 	{
 		"pic": "<img src='15.png'>",
 		"name": "Quarantine Day: 15",
-		"desc": "No comment";
+		"desc": "No comment",
 
 		
 	},
@@ -119,7 +119,7 @@ var photos = [
 	{
 		"pic": "<img src='16.png'>",
 		"name": "Quarantine Day: 16",
-		"desc": "When you know no one can see your drawing but you're trying your best";
+		"desc": "When you know no one can see your drawing but you're trying your best",
 
 		
 	},
@@ -127,7 +127,7 @@ var photos = [
 	{
 		"pic": "<img src='17.png'>",
 		"name": "Quarantine Day: 17",
-		"desc": "Ｔｈｅ ｂｅｓｔ ｂａｎｄ ｅｖｅｒ， ｅｖｅｒｙｏｎｅ ａｇｒｅｅｓ";
+		"desc": "Ｔｈｅ ｂｅｓｔ ｂａｎｄ ｅｖｅｒ， ｅｖｅｒｙｏｎｅ ａｇｒｅｅｓ",
 
 		
 	},
