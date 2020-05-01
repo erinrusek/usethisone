a23d4ed287eaeb8de02695fa42bee234b701ddda
Your Name <you@example.com>
2020-04-30 23:25:44 -0500

    the JAVASCRIPT IS WORKINGgit statusgit status

--- labweek11/main.js
+++ labweek11/main.js
@@ -22,7 +22,7 @@ var photos = [
 		"pic": "<img src='3.png'>",
 		"name": "Quarantine Day: 3",
 		"desc": "Zoom calling friends for social interaction and background image battles ensue....",
-
+		"news": "February 11th, 2020: ‘Like Europe in Medieval Times’: Virus Slows China’s Economy",
 
 	},
 
@@ -30,6 +30,7 @@ var photos = [
 		"pic": "<img src='4.png'>",
 		"name": "Quarantine Day: 4",
 		"desc": "Zoom calling friends for social interaction and background image battles ensue....",
+		"news": "February 19th, 2020: Coronavirus Updates: Economic Fallout Widens",
 
 		
 	},
@@ -38,141 +39,171 @@ var photos = [
 		"pic": "<img src='5.png'>",
 		"name": "Quarantine Day: 5",
 		"desc": "Zoom calling friends for social interaction and background image battles ensue....",
-		
+		"news": "February 25th, 2020: Milan, Italy’s economic engine, is eerily quiet amid a surge in cases. Here’s the latest.",
 	},
 
 	{
 		"pic": "<img src='6.png'>",
-		"name": "Quarantine Day: 6",
+		"name": "Quarantine Day: 10",
 		"desc": "Zoom calling friends for social interaction and background image battles ensue....",
+		"news": "February 29th, 2020: A Second Case in California Raises New Fears of Local Spread: Updates",
 		
 	},
 
 	{
 		"pic": "<img src='7.png'>",
-		"name": "Quarantine Day: 7",
+		"name": "Quarantine Day: 22",
 		"desc": "Roon knows about the time you used CSS kayframes instead of linking to JavaScript to create your time-based animation",
+		"news": "March 3rd, 2020: Coronavirus Outbreak Strikes Seattle Area as Global Epidemic Spreads",
 		
 	},
 
 	{
 		"pic": "<img src='8.png'>",
-		"name": "Quarantine Day: 8",
+		"name": "Quarantine Day: 37",
 		"desc": "Qu'est-ce que c'est???",
+		"news": "March 8th, 2020: New York Declares State of Emergency",
 
 		
 	},
 
 	{
 		"pic": "<img src='9.png'>",
-		"name": "Quarantine Day: 9",
+		"name": "Quarantine Day: 100",
 		"desc": "Cursed image radiating more meta energy (mirror images on the 2nd and last participant screens)",
+		"news": "March 13th, 2020: Stocks Plunge by the Most Since 1987: Live Updates",
 
 		
 	},
 
 	{
 		"pic": "<img src='10.png'>",
-		"name": "Quarantine Day: 10",
+		"name": "Quarantine Day: 122",
 		"desc": "No comment",
+		"news": "March 16th, 2020: Europeans Erect Borders Against Virus, but the Enemy Is Already Within: In societies where life plays out in public, where friends are greeted with kisses on the cheeks, there is fear and fragmentation.",
 
 	},
 
 	{
 		"pic": "<img src='11.png'>",
-		"name": "Quarantine Day: 11",
+		"name": "Quarantine Day: 1111",
 		"desc": "The discovery of the 'I have a green screen' effect on Zoom",
+		"news": "March 19th, 2020: Younger Adults Comprise Big Portion of Virus Hospitalizations in U.S.",
 
 		
 	},
 
 	{
 		"pic": "<img src='12.png'>",
-		"name": "Quarantine Day: 12",
+		"name": "Quarantine Day: 1278",
 		"desc": "Which ones are from the South?",
+		"news": "March 24th, 2020: Britain Ordered to Stay Indoors; Senators Split Over $1.8 Trillion Aid Package",
 
 		
 	},
 
 	{
 		"pic": "<img src='13.png'>",
-		"name": "Quarantine Day: 13",
+		"name": "Quarantine Day: 139098",
 		"desc": "No comment",
+		"news": "March 26th, 2020: 13 Deaths in a Day: An ‘Apocalyptic’ Surge at an N.Y.C. Hospital",
 
 		
 	},
 
 	{
 		"pic": "<img src='14.png'>",
-		"name": "Quarantine Day: 14",
+		"name": "Quarantine Day: 142864092",
 		"desc": "The discovery of dad backgrounds holds infinite potential",
+		"news": "March 29th, 2020: U.S. Cases Cross 119,000: N.Y.C.’s 911 System Is Overwhelmed. ‘I’m Terrified,’ a Paramedic Says.",
 
 	},
 
 	{
 		"pic": "<img src='15.png'>",
-		"name": "Quarantine Day: 15",
+		"name": "Quarantine Day: 15934602",
 		"desc": "No comment",
+		"news": "March 31st, 2020: Despite Pushback, Trump Suggests Testing Is No Longer an Issue",
 
 		
 	},
 
 	{
 		"pic": "<img src='16.png'>",
-		"name": "Quarantine Day: 16",
+		"name": "Quarantine Day: 1691284610",
 		"desc": "When you know no one can see your drawing but you're trying your best",
+		"news": "April 1st, 2020: Trump Warns of a ‘Very, Very Painful Two Weeks’ Ahead",
 
 		
 	},
 
 	{
 		"pic": "<img src='17.png'>",
-		"name": "Quarantine Day: 17",
+		"name": "Quarantine Day: 171982640283",
 		"desc": "Ｔｈｅ ｂｅｓｔ ｂａｎｄ ｅｖｅｒ， ｅｖｅｒｙｏｎｅ ａｇｒｅｅｓ",
+		"news": "April 4th, 2020: C.D.C. Recommends Americans Wear Cloth Masks in Public",
 
 		
 	},
 
 	{
 		"pic": "<img src='18.png'>",
-		"name": "Quarantine Day: 18",
+		"name": "Quarantine Day: 18329840328654",
+
+		"news": "April 9th, 2020: Global: W.H.O. Chief Warns Against Politicizing Virus",
 		
 	},
 
 	{
 		"pic": "<img src='19.jpg'>",
-		"name": "Quarantine Day: 19",
+		"name": "Quarantine Day: 1929041863314373",
+
+		"news": "April 13th, 2020: Global updates: Lockdowns in Europe are shifting, and Prime Minister Boris Johnson was released from the hospital.",		
 		
 	},
 
 	{
 		"pic": "<img src='20.png'>",
-		"name": "Quarantine Day: 20",
+		"name": "Quarantine Day: 20237840146374591",
+
+		"news": "April 17th, 2020: Trump Tells Governors to ‘Call Your Own Shots’ on When to Reopen",
+
 		
 	},
 
 	{
 		"pic": "<img src='21.png'>",
-		"name": "Quarantine Day: 21",
+		"name": "Quarantine Day: 21202378401463745913y4?",
 		"desc": "No comment",
+		"news": "April 21st, 2020: ‘I Don’t Think the New York That We Left Will Be Back for Some Years’",
 
 		
 	},
 
 	{
 		"pic": "<img src='22.png'>",
-		"name": "Quarantine Day: 22",
+		"name": "Quarantine Day: 2220237840146374591823150139864???",
 		"desc": "No comment",
+		"news": "April 23rd, 2020: Virus Vaccine Chief Ousted: Says He Questioned a Drug Trump Praised",
 
 		
 	},
 
 	{
 		"pic": "<img src='23.png'>",
-		"name": "Quarantine Day: 23",
+		"name": "Quarantine Day: 2320237840146374591327418341923745912374?????",
 		"desc": "Ted Nelson is STILL ALIVE according to Wikipedia",
+		"news": "April 26th, 2020: Testing Remains Scarce as Governors Weigh Reopening States",
 
 		
-	}
+	},
+	{
+		"pic": "<img src='24.png'>",
+		"name": "Quarantine Day: HELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELPHELP",
+		"desc": "What's that noise?! I....should check back to the living room",
+		"news": "",
+
+		
+	},
 
 ]
